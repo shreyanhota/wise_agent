44adc8a6c6b45fba020ca1a81d31666ae3abd171
shreyanhota <shreyanhota@gmail.com>
2025-02-26 15:29:03 +0530

    First Commit

---
+++ twilio_init.py
@@ -0,0 +1,23 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+
+account_sid = "ACcae4454ffa2c5db7f5f368cec0ef4af0"
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+  url="http://demo.twilio.com/docs/voice.xml",
+  to="+919777256250",
+  from_="+19895192066"
+)
+
+print(call.sid)
+
